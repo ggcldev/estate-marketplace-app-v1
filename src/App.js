37d0a5ed80c20e10f6c429f5df2e8ac9bd8a7975
ggcldev <ggcldev@gmail.com>
2022-12-05 18:27:10 +0800

    Created Router; Created Pages, assign path to Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Discover from './pages/Discover'
+import Sale from './pages/Sale'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import ForgotPassword from './pages/ForgotPassword'
 
 function App() {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
-      </div>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Discover />} />
+          <Route path='/sales' element={<Sale />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
---
+++ src/pages/Discover.jsx
@@ -0,0 +1,5 @@
+function Discover() {
+  return <h1>Discovery</h1>
+}
+
+export default Discover
